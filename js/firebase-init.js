462977c65fc2a1eceb0c6f820bba2be29c1efcf7
BananaBox69 <androth1989@gmail.com>
2025-07-21 23:49:28 +0200

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,13 @@
+<!-- Firebase SDKs -->
+<script src="https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js"></script>
+
+<!-- Application Scripts (LOAD IN THIS ORDER) -->
+<script src="/js/firebase-init.js"></script>
+<script src="/js/utils.js"></script>
+<script src="/js/themes.js"></script>
+<script src="/js/cards.js"></script>
+<script src="/js/admin.js"></script>
+<script src="/js/modals.js"></script>
+<script src="/js/ui.js"></script>
+<script src="/js/app.js"></script>
