8f8cf2ae8b99363fd32eedf3abae73cebee768b5
BananaBox69 <androth1989@gmail.com>
2025-07-22 00:02:10 +0200

    Delete js directory

--- js/ui.js
+++
@@ -1,7 +0,0 @@
-// Remove all imports
-
-window.initUI = function() {
-  // Setup event listeners
-  document.getElementById('search').addEventListener('input', displayCards);
-  // ... other listeners ...
-};
