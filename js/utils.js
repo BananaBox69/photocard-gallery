b72f64695016fa51bf863f8391750c9830c5725f
BananaBox69 <androth1989@gmail.com>
2025-07-21 23:51:28 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,32 +1,18 @@
-// Constants
-const MEMBER_COLORS = {
-  'irene': '#F8C8DC', 'seulgi': '#FFFACD', 'wendy': '#A7C7E7', 
-  'joy': '#C1E1C1', 'yeri': '#D8BFD8', 'iu': '#E6E6FA',
-  'karina': '#ADD8E6', 'giselle': '#F5F5F5', 'winter': '#E0FFFF', 
-  'ningning': '#FFB6C1'
+// Remove any 'export' or 'import' statements
+var MEMBER_COLORS = {
+  'irene': '#F8C8DC', 
+  'seulgi': '#FFFACD',
+  // ... rest of your colors
 };
 
-const GROUP_ABBR = { 'Red Velvet': 'RV', 'IU': 'IU', 'aespa': 'AE' };
-const GROUP_ORDER = ['Red Velvet', 'IU', 'aespa'];
-const MEMBER_ORDER = ['Irene', 'Seulgi', 'Wendy', 'Joy', 'Yeri', 'IU', 'Karina', 'Giselle', 'Winter', 'Ningning'];
+var GROUP_ABBR = { 'Red Velvet': 'RV', 'IU': 'IU', 'aespa': 'AE' };
+// ... rest of your constants
 
-const ARTIST_DATA = {
-  "Red Velvet": {
-    members: ["Irene", "Seulgi", "Wendy", "Joy", "Yeri"],
-    albums: ["Ice Cream Cake", "The Red", "The Velvet", /* ... */]
-  },
-  /* ... other artist data ... */
+// Make functions global
+window.darkenColor = function(hex, percent) {
+  // ... implementation ...
 };
 
-const COUNTRY_LIST = ["Afghanistan", "Albania", /* ... */];
-
-const ADMIN_PASSWORD = "4649";
-
-// Utility Functions
-function darkenColor(hex, percent) {
+window.calculateDiscountedPrice = function(price, type) {
   // ... implementation ...
-}
-
-function calculateDiscountedPrice(price, type) {
-  // ... implementation ...
-}
+};
